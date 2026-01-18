71bb4d035f84b81e2fe09ee733ab2d376822ec9e
lucky <luckyvirani555@gmail.com>
2026-01-18 10:45:07 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import HeroSection from "@/components/hero/HeroSection";
 import Navbar from "@/components/layout/Navbar";
 import FeaturedProperties from "@/components/property/FeaturedProperties";
 import StatsSection from "@/components/sections/StatsSection";
-import { TestimonialsSection } from "@/components/testimonials";
 import Footer from "@/components/layout/Footer";
 
 export default function Home() {
